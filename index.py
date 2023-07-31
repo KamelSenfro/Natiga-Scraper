d2a97fbf58494fc2f833497c1a704815c2f91b66
KamelSenfro <kamel.fares02@eng-st.cu.edu.eg>
2023-07-31 14:58:32 +0300

    fixing-bugs

--- index.py
+++ index.py
@@ -1,13 +1,13 @@
-import csv
 import requests
 from bs4 import BeautifulSoup
-import time
+
 def scrape_data(seat_number):
     url = "https://natega.cairo24.com/"
 
     # Prepare the data to be sent in the POST request
     data = {
         "seatNo": seat_number,
+        "submit": "النتيجة"
     }
 
     # Send the POST request to get the results
@@ -17,36 +17,24 @@ def scrape_data(seat_number):
     soup = BeautifulSoup(response.content, "html.parser")
 
     # Find the element containing the desired data
-    result_element = soup.find("li", class_="col resultItem")
-
-    if result_element:
-        # Extract the required information
-        seat_no = result_element.h1.text.strip()
-        return seat_no
+    name_element = soup.find("span", class_="formatt active", text="الأسم:")
+    if name_element:
+        name = name_element.find_next_sibling("span").text.strip()
+        return name
     else:
         return None
 
-def save_to_csv(data_list):
-    with open("scraped_data.csv", mode="w", newline="") as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(["Seat Number"])
-        for item in data_list:
-            writer.writerow([item])
-
 if __name__ == "__main__":
-    start_seat_number = 1391446
-    end_seat_number = 1391465
-    seat_numbers = list(range(start_seat_number, end_seat_number + 1))
+    seat_numbers = list(range(1391446, 1391466))  # Replace with your desired seat numbers
     scraped_data = []
 
     for seat_number in seat_numbers:
-    result = scrape_data(seat_number)
-    if result:
-        scraped_data.append(result)
-    time.sleep(1)  # Add a 1-second delay between each request
+        result = scrape_data(seat_number)
+        if result:
+            scraped_data.append(result)
 
     if scraped_data:
-        save_to_csv(scraped_data)
-        print("Data scraped and saved to 'scraped_data.csv'")
+        for seat_number, name in zip(seat_numbers, scraped_data):
+            print(f"Seat Number: {seat_number} - Name: {name}")
     else:
         print("No data found.")
